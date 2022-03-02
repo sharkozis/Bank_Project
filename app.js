45b16fccdc7fda678c97ba227e7c69a0475e6465
sharkozis <hossicoder@gmail.com>
2022-03-02 11:39:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click',function(){
+
+    // for getting user Email address
+    const userEmail = document.getElementById('user-email');
+    const getUserEmail = userEmail.value;
+    userEmail.value = '';
+    // console.log(getUserEmail);
+
+    // for getting user password
+    const userPassword = document.getElementById('user-password');
+    const getUserPassword = userPassword.value;
+    userPassword.value = '';
+    // console.log(getUserPassword);
+
+    // Validity Checking
+    if(getUserEmail == 'abc@gmail.com' && getUserPassword == 'secret'){
+       window.location.href= 'Banking.html'
+    }
+})
